f5f7b0293daf04de2e0d33ef15124b945beae220
lindeneg <christian@lindeneg.org>
2024-02-11 14:34:01 +0100

    update

--- nvim/lua/lindeneg/run-cmd.lua
+++ nvim/lua/lindeneg/run-cmd.lua
@@ -2,14 +2,14 @@ vim.api.nvim_create_user_command(
 "Compile",
 function()
     vim.cmd("below split")
-    vim.cmd("term " .. "C:\\Users\\chris\\clvs.bat")
+    vim.cmd("term")
 
     vim.fn.feedkeys("a")
 
     local enter = vim.api.nvim_replace_termcodes("<CR>", true, true, true)
 
-    vim.fn.feedkeys("clear" .. enter)
-    vim.fn.feedkeys("b" .. enter)
+    vim.fn.feedkeys("cls" .. enter)
+    vim.fn.feedkeys(".\\misc\\build.bat" .. enter)
 end,
 { nargs = 0 }
 )
